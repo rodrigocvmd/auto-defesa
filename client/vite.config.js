9c0f0dce56a671e3cfd0b0c81db54eb9c0c129a4
Rodrigo Carvalho <rodrigo010carvalho@gmail.com>
2026-02-09 11:04:31 -0300

    chore: implement automatic sitemap generation in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+import { articles } from './src/data/articles'
+
+const dynamicRoutes = [
+  ...articles.map(article => `/artigo/${article.slug}`),
+  '/motorista-app',
+  '/caminhoneiro',
+  '/motoqueiro',
+  '/ppd',
+  '/login',
+  '/register',
+  '/pricing',
+  '/how-it-works',
+  '/about',
+  '/help',
+  '/guia',
+  '/recursos',
+  '/terms',
+  '/privacy'
+]
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://meuautodefesa.com.br',
+      dynamicRoutes
+    })
+  ],
 })
